fa0c8b14c9d4a6730b3d996b4b69d8a5474792e7
xx707674485 <707674485@qq.com>
2018-03-08 17:28:00 +0800

    Initial commit

---
+++ app/src/main/java/com/xjst/skysweety/TestActivity.java
@@ -0,0 +1,12 @@
+package com.xjst.skysweety;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 作者：zx on 2018/3/8 16:46
+ * 描述:
+ */
+
+
+public class TestActivity extends AppCompatActivity {
+}
